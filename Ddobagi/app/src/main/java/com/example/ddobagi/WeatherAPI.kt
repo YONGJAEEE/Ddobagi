ca7d683142baa20e4ba6864f18fc34b7d0723290
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2020-10-22 17:35:21 +0900

    프로젝트 파일 재업로드

--- Ddobagi/app/src/main/java/com/example/ddobagi/WeatherAPI.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ddobagi
-
-import android.service.autofill.UserData
-import android.telecom.Call
-import androidx.room.Query
-import retrofit2.http.GET
-
-interface WeatherAPI {
-}
